be74c508b70b6d91b991eb059899b43a78a9045b
KatsuMouley <130433332+KatsuMouley@users.noreply.github.com>
2023-12-06 11:22:00 -0300

    background correction

--- JS/main.js
+++ JS/main.js
@@ -5,10 +5,10 @@ document.addEventListener("DOMContentLoaded", function () {
     botDivs.forEach(function (botDiv) {
         botDiv.addEventListener('click', function () {
             var botId = botDiv.id;
-            console.log('Bot clicado: ' + botId);
+
             changeBackgroundImage(botId);
         });
-//        botDiv.classList.add('CurrentBot');
+        //        botDiv.classList.add('CurrentBot');
     });
 });
 
@@ -17,19 +17,21 @@ function changeBackgroundImage(botId) {
     var imageUrl = getImageUrl(botId);
 
     // Altera o src da imagem com a classe BGimg
-    var bgImage = document.querySelector('.BGimg');
-    bgImage.src = imageUrl;
+    var bgImage = document.body;
+    bgImage.style = `background:${imageUrl}; height: 100%; background-attachment: fixed; 
+    background-color: #4e4e4e; background-position: center; background-repeat: no-repeat;
+    background-size: cover; z-index: -1;`;
 }
 
 function getImageUrl(botId) {
     // Mapeia os IDs dos bots para suas respectivas URLs de imagem
     var imageUrls = {
-        'catbotimg': 'css/images/kittenBOG.jpg',
-        'duckbot': 'css/images/duck picture.jpg',
-        'dogbot': 'css/images/Puppy.jpg',
-        'chaddog': 'css/images/SigmaDog.jpg',
-        'crazycat': 'css/images/kitten.jpg',
-        'uglyduck': 'css/images/prupru.jpg'
+        'catbotimg': "linear-gradient(rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.7)), url('css/images/kittenBOG.jpg')",
+        'duckbot': "linear-gradient(rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.7)), url('css/images/duck picture.jpg')",
+        'dogbot': "linear-gradient(rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.7)), url('css/images/Puppy.jpg')",
+        'chaddog': "linear-gradient(rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.7)), url('css/images/SigmaDog.jpg')",
+        'crazycat': "linear-gradient(rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.7)), url('css/images/kitten.jpg')",
+        'uglyduck': "linear-gradient(rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.7)), url('css/images/prupru.jpg')"
     };
 
     // Retorna a URL da imagem correspondente ao bot clicado
